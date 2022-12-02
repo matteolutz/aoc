d7fbc40f57a20600c3ac31bd551fa7d070946437
matteolutz <matteo.lutz06@outlook.de>
2022-12-02 10:51:51 +0100

    day2

--- aoc_2022/src/day2/mod.rs
+++ aoc_2022/src/day2/mod.rs
@@ -1,14 +1,26 @@
-#[derive(Copy, Clone, Debug)]
+#[derive(Debug)]
 pub enum Move {
     Rock,
     Paper,
     Scissors
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Debug)]
+pub enum PlayerMove {
+    // ROCk
+    X,
+
+    // PAPER
+    Y,
+
+    // SCISSORS
+    Z
+}
+
+#[derive( Debug)]
 pub struct Strategy {
-    opponent: Move,
-    my_move: Move,
+    opponent_move: Move,
+    my_move_or_outcome: PlayerMove,
 }
 
 #[aoc_generator(day2)]
@@ -24,25 +36,92 @@ pub fn input_strategies(input: &str) -> Vec<Strategy> {
                 "C" => Move::Scissors,
                 _ => panic!("Invalid move"),
             };
-            let my_moe = match parts[1] {
-                "X" => Move::Rock,
-                "Y" => Move::Paper,
-                "Z" => Move::Scissors,
+            let my_move = match parts[1] {
+                "X" => PlayerMove::X,
+                "Y" => PlayerMove::Y,
+                "Z" => PlayerMove::Z,
                 _ => panic!("Invalid move"),
             };
             Strategy {
-                opponent: opponent_move,
-                my_move: my_moe,
+                opponent_move: opponent_move,
+                my_move_or_outcome: my_move,
             }
         }).collect()
 }
 
-pub fn calculate_round(strategy: &Strategy) -> i32 {
-    // TODO
-    0
+pub fn get_outcome(my_move: &PlayerMove, opponent_move: &Move) -> i32 {
+    match opponent_move {
+        Move::Rock => match my_move {
+            PlayerMove::X => 0,
+            PlayerMove::Y => 1,
+            PlayerMove::Z => -1,
+        },
+        Move::Paper => match my_move {
+            PlayerMove::X => -1,
+            PlayerMove::Y => 0,
+            PlayerMove::Z => 1,
+        },
+        Move::Scissors => match my_move {
+            PlayerMove::X => 1,
+            PlayerMove::Y => -1,
+            PlayerMove::Z => 0,
+        },
+    }
+}
+
+pub fn calculate_round(my_move: &PlayerMove, opponent_move: &Move) -> i32 {
+    let shape_score = match my_move {
+        PlayerMove::X => 1,
+        PlayerMove::Y => 2,
+        PlayerMove::Z => 3,
+    };
+    let outcome_score = match get_outcome(my_move, opponent_move) {
+        1 => 6,
+        0 => 3,
+        -1 => 0,
+        _ => panic!("Invalid outcome"),
+    };
+    shape_score + outcome_score
 }
 
 #[aoc(day2, part1)]
 pub fn part1(strategies: &[Strategy]) -> i32 {
-    0
+    strategies.iter()
+        .fold(0, |acc, strategy| acc + calculate_round(&strategy.my_move_or_outcome, &strategy.opponent_move))
+}
+
+pub fn get_loose_move(m: &Move) -> PlayerMove {
+    match m {
+        Move::Rock => PlayerMove::Z,
+        Move::Paper => PlayerMove::X,
+        Move::Scissors => PlayerMove::Y,
+    }
+}
+
+pub fn get_tie_move(m: &Move) -> PlayerMove {
+    match m {
+        Move::Rock => PlayerMove::X,
+        Move::Paper => PlayerMove::Y,
+        Move::Scissors => PlayerMove::Z,
+    }
+}
+
+pub fn get_win_move(m: &Move) -> PlayerMove {
+    match m {
+        Move::Rock => PlayerMove::Y,
+        Move::Paper => PlayerMove::Z,
+        Move::Scissors => PlayerMove::X,
+    }
+}
+
+#[aoc(day2, part2)]
+pub fn part2(strategies: &[Strategy]) -> i32 {
+    strategies.iter()
+        .fold(0, |acc, strategy| {
+            acc + match strategy.my_move_or_outcome {
+                PlayerMove::X => calculate_round(&get_loose_move(&strategy.opponent_move), &strategy.opponent_move),
+                PlayerMove::Y => calculate_round(&get_tie_move(&strategy.opponent_move), &strategy.opponent_move),
+                PlayerMove::Z => calculate_round(&get_win_move(&strategy.opponent_move), &strategy.opponent_move),
+            }
+        })
 }
--- aoc_2022/src/lib.rs
+++ aoc_2022/src/lib.rs
@@ -2,6 +2,7 @@ extern crate aoc_runner;
 
 #[macro_use]
 extern crate aoc_runner_derive;
+extern crate core;
 
 pub mod day1;
 pub mod day2;
