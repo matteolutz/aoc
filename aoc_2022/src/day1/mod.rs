e7e8d06778e0b3ccafc15d30dde765131ad82c79
matteolutz <matteo.lutz06@outlook.de>
2022-12-02 07:31:56 +0100

    code beautify day1

--- aoc_2022/src/day1/mod.rs
+++ aoc_2022/src/day1/mod.rs
@@ -1,45 +1,23 @@
 #[aoc_generator(day1)]
 pub fn input_calories(input: &str) -> Vec<Vec<i32>> {
-    let mut calories = Vec::new();
-
-    let mut current_elve = Vec::new();
-    for l in input.lines() {
-        if l.is_empty() {
-            calories.push(current_elve);
-            current_elve = Vec::new();
-        } else {
-            current_elve.push(l.parse().unwrap());
-        }
-    }
-
-    calories
+    input
+        .split("\n\n")
+        .filter(|s| !s.is_empty())
+        .map(|s| s.split('\n').map(|s| s.parse().unwrap()).collect())
+        .collect::<Vec<Vec<i32>>>()
 }
 
 #[aoc(day1, part1)]
 pub fn part1(calories: &[Vec<i32>]) -> i32 {
-    let mut max = 0;
-    for elve in calories {
-        let mut total = 0;
-        for c in elve {
-            total += c;
-        }
-        if total > max {
-            max = total;
-        }
-    }
-    max
+    calories.iter().map(|elve| elve.iter().sum()).max().unwrap()
 }
 
 #[aoc(day1, part2)]
 pub fn part2(calories: &[Vec<i32>]) -> i32 {
-    let mut elves_total = Vec::new();
-    for elve in calories {
-        let mut total = 0;
-        for c in elve {
-            total += c;
-        }
-        elves_total.push(total);
-    }
+    let mut elves_total = calories
+        .iter()
+        .map(|elve| elve.iter().sum())
+        .collect::<Vec<i32>>();
 
     elves_total.sort();
     elves_total.reverse();
