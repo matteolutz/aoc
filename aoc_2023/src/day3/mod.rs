df3fb0d0cc1b2c24871bd0494203a2f4b0bde251
matteolutz <matteo.lutz06@outlook.de>
2023-12-03 18:36:48 +0100

    day3, part2

--- aoc_2023/src/day3/mod.rs
+++ aoc_2023/src/day3/mod.rs
@@ -13,60 +13,72 @@ pub fn is_symbol(c: &char) -> bool {
     res
 }
 
-pub fn flip_number(n: u32) -> u32 {
-    let mut flipped = 0_u32;
-    let mut n = n;
-    while n > 0 {
-        flipped *= 10;
-        flipped += n % 10;
-        n /= 10;
-    }
-
-    flipped
-}
-
 pub fn has_adjacent_symbol(schematic: &EngineSchematic, x: usize, y: usize) -> (bool, usize, usize) {
-    // left
-    if x > 0 && is_symbol(&schematic[y][x - 1]) {
-        return (true, x - 1, y);
-    }
+    for dy in -1..=1 {
+        for dx in -1..=1 {
+            if dx == 0 && dy == 0 {
+                continue;
+            }
 
-    // right
-    if x < schematic[y].len() - 1 && is_symbol(&schematic[y][x + 1]) {
-        return (true, x + 1, y);
-    }
+            let x = x as i32 + dx;
+            let y = y as i32 + dy;
+            if x < 0 || y < 0 || y >= schematic.len() as i32 || x >= schematic[y as usize].len() as i32 {
+                continue;
+            }
 
-    // above
-    if y > 0 && is_symbol(&schematic[y - 1][x]) {
-        return (true, x, y - 1);
-    }
-
-    // below
-    if y < schematic.len() - 1 && is_symbol(&schematic[y + 1][x]) {
-        return (true, x, y + 1);
-    }
-
-    // diagonally left above
-    if x > 0 && y > 0 && is_symbol(&schematic[y - 1][x - 1]) {
-        return (true, x - 1, y - 1);
-    }
-
-    // diagonally right above
-    if x < schematic[y].len() - 1 && y > 0 && is_symbol(&schematic[y - 1][x + 1]) {
-        return (true, x + 1, y - 1);
-    }
-
-    // diagonally left below
-    if x > 0 && y < schematic.len() - 1 && is_symbol(&schematic[y + 1][x - 1]) {
-        return (true, x - 1, y + 1);
-    }
-
-    // diagonally right below
-    if x < schematic[y].len() - 1 && y < schematic.len() - 1 && is_symbol(&schematic[y + 1][x + 1]) {
-        return (true, x + 1, y + 1);
+            if is_symbol(&schematic[y as usize][x as usize]) {
+                return (true, x as usize, y as usize);
+            }
+        }
     }
 
     (false, 0, 0)
+}
+
+pub fn find_adjacent_numbers(schematic: &EngineSchematic, x: usize, y: usize) -> Vec<u32> {
+    let mut numbers = Vec::<u32>::new();
+
+    for dy in -1..=1 {
+        for dx in -1..=1 {
+            if dx == 0 && dy == 0 {
+                continue;
+            }
+
+            let x = x as i32 + dx;
+            let y = y as i32 + dy;
+            if x < 0 || y < 0 || y >= schematic.len() as i32 || x >= schematic[y as usize].len() as i32 {
+                continue;
+            }
+
+            if schematic[y as usize][x as usize].is_numeric() {
+                let (number, _, _) = get_number(schematic, x as usize, y as usize);
+                if numbers.contains(&number) {
+                    // a bit of a hack, but it works
+                    continue;
+                }
+                numbers.push(number);
+            }
+        }
+    }
+
+    numbers
+}
+
+pub fn get_number(schematic: &EngineSchematic, x: usize, y: usize) -> (u32, usize, usize) {
+    let mut number = 0_u32;
+    let mut first_digit_x = x;
+
+    while first_digit_x > 0 && schematic[y][first_digit_x - 1].is_numeric() {
+        first_digit_x -= 1;
+    }
+
+    let mut current_digit_x = first_digit_x;
+    while current_digit_x < schematic[y].len() && schematic[y][current_digit_x].is_numeric() {
+        number = number * 10 + schematic[y][current_digit_x].to_digit(10).unwrap();
+        current_digit_x += 1;
+    }
+
+    (number, first_digit_x, y)
 }
 
 #[aoc(day3, part1)]
@@ -113,6 +125,28 @@ pub fn part1(schematic: &EngineSchematic) -> u32 {
     total
 }
 
+#[aoc(day3, part2)]
+pub fn part2(input: &EngineSchematic) -> u32 {
+    let mut total = 0_u32;
+
+    for (y, row) in input.iter().enumerate() {
+        for (x, c) in row.iter().enumerate() {
+            if *c != '*' {
+                continue;
+            }
+
+            let adj = find_adjacent_numbers(input, x, y);
+            if adj.len() != 2 {
+                continue;
+            }
+
+            total += adj.iter().product::<u32>();
+        }
+    }
+
+    total
+}
+
 #[cfg(test)]
 mod tests {
     use crate::day3::input_schematic;
@@ -131,4 +165,21 @@ mod tests {
 .664.598..");
         assert_eq!(super::part1(&schematic), 4361);
     }
+
+    #[test]
+    fn test_part_2() {
+
+        let schematic = input_schematic("467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..");
+        assert_eq!(super::part2(&schematic), 467835);
+    }
+
 }
